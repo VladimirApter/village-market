c8ad37f1b25fc7467fdba72f3576228cbbdd6d57
VladimirApter <vova.apter@gmail.com>
2024-05-25 21:33:23 +0500

    Change request compliance

--- village market/Assets/Scripts/Check Request.cs
+++ village market/Assets/Scripts/Check Request.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Model;
 using UnityEngine;
 
@@ -23,7 +24,7 @@ public class CheckRequest : MonoBehaviour
             foreach (var fruit in request.FruitsCount.Keys)
             {
                 var tableFruitCount = table.FruitsCount[fruit];
-                if (request.FruitsCount[fruit] != tableFruitCount)
+                if (request.FruitsCount[fruit] > tableFruitCount)
                     isRequestCompleted = false;
             }
             if (!isRequestCompleted) 
@@ -31,15 +32,37 @@ public class CheckRequest : MonoBehaviour
             Player.TotalScore += request.Price;
             TutorialScript.isTutorialFinished = true;
 
-            foreach (var fruit in table.Fruits)
+            var fruitsToDestroy = new List<Fruit>();
+
+            foreach (var (fruitName, destroyCount) in request.FruitsCount)
             {
+                for (var i = 0; i < destroyCount; i++)
+                {
+                    foreach (var fruit in table.Fruits)
+                    {
+                        if (fruitsToDestroy.Contains(fruit)) continue;
+                        
+                        var isFruitDetected = false;
+                        if ((fruitName == "beet" && fruit is Beet) || (fruitName == "wheat" && fruit is Wheat) ||
+                            (fruitName == "apple" && fruit is Apple) || fruitName == "fruit")
+                        {
+                            table.FruitsCount[fruitName]--;
+                            fruitsToDestroy.Add(fruit);
+                            isFruitDetected = true;
+                        }
+
+                        if (isFruitDetected) break;
+                    }
+                }
+            }
+            foreach (var fruit in fruitsToDestroy)
+            {
+                table.Fruits.Remove(fruit);
                 Objects.Fruits.Remove(fruit);
                 Objects.Things.Remove(fruit);
                 Destroy(fruit.ThingObj);
             }
-            table.Fruits.Clear();
-            table.FruitsCount = new Dictionary<string, int> { { "beet", 0 }, { "wheat", 0 }, { "apple", 0 }, { "fruit", 0 } };
-
+            
             foreach (var fruit in request.Fruits)
                 Destroy(fruit);
             Objects.Requests.Remove(requestCoords);
