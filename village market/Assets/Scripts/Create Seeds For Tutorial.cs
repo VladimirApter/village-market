35cb3613364d7eb9b578f92347e9a1e2c2fb9ace
VladimirApter <vova.apter@gmail.com>
2024-05-25 23:59:30 +0500

    Add infinity seeds, change score style

--- village market/Assets/Scripts/Create Seeds For Tutorial.cs
+++ village market/Assets/Scripts/Create Seeds For Tutorial.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Model;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -8,29 +9,29 @@ using UnityEngine.Serialization;
 public class CreateSeedsForTutorial : MonoBehaviour
 {
     public GameObject seedsObjs;
-    public int seedsCount = 1;
-    private GameObject platform;
+    public int seedsCount = 20;
     
     // Start is called before the first frame update
     void Start()
     {
-        platform = Platform.PlatformObj;
-        var seeds = new List<Seed>();
         
-        for (var i = 0; i < seedsCount; i++)
-        {
-            seeds.Add(new WheatSeed()
-            {
-                ThingObj = Instantiate(WheatSeed.WheatSeedPrefab, SquareSection.ConvertSectionToVector((-2, 4)),//GetRandomPlatformPosition(),
-                    Quaternion.identity, seedsObjs.transform),
-            });
-        }
-
-        Objects.Things.AddRange(seeds);
     }
 
     // Update is called once per frame
     void Update()
     {
+        var seeds = Objects.Things.Where(t => t is Seed).ToArray();
+        var seedsToAdd = new List<Seed>();
+        
+        for (var i = 0; i < seedsCount - seeds.Count(s => s is WheatSeed); i++)
+        {
+            seedsToAdd.Add(new WheatSeed()
+            {
+                ThingObj = Instantiate(WheatSeed.WheatSeedPrefab, SquareSection.ConvertSectionToVector((-2, 4)),
+                    Quaternion.identity, seedsObjs.transform),
+            });
+        }
+        
+        Objects.Things.AddRange(seedsToAdd);
     }
 }
--- village market/Assets/Scripts/Create Seeds.cs
+++ village market/Assets/Scripts/Create Seeds.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Model;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -8,47 +9,47 @@ using UnityEngine.Serialization;
 public class CreateSeeds : MonoBehaviour
 {
     public GameObject seedsObjs;
-    private int beetSeedsCount = 10;
-    private int wheatSeedsCount = 10;
-    private int appleTreeSeedsCount = 10;
+    private int beetSeedsCount = 20;
+    private int wheatSeedsCount = 20;
+    private int appleTreeSeedsCount = 20;
     
     // Start is called before the first frame update
     void Start()
     {
-        var seeds = new List<Seed>();
         
-        for (var i = 0; i < beetSeedsCount; i++)
-        {
-            seeds.Add(new BeetSeed()
-            {
-                ThingObj = Instantiate(BeetSeed.BeetSeedPrefab, SquareSection.ConvertSectionToVector((-4, 4)),
-                    Quaternion.identity, seedsObjs.transform),
-            });
-        }
-        
-        for (var i = 0; i < wheatSeedsCount; i++)
-        {
-            seeds.Add(new WheatSeed()
-            {
-                ThingObj = Instantiate(WheatSeed.WheatSeedPrefab, SquareSection.ConvertSectionToVector((-2, 4)),
-                    Quaternion.identity, seedsObjs.transform),
-            });
-        }
-        
-        for (var i = 0; i < appleTreeSeedsCount; i++)
-        {
-            seeds.Add(new AppleTreeSeed()
-            {
-                ThingObj = Instantiate(AppleTreeSeed.AppleTreeSeedPrefab, SquareSection.ConvertSectionToVector((-1, 4)),
-                    Quaternion.identity, seedsObjs.transform),
-            });
-        }
-
-        Objects.Things.AddRange(seeds);
     }
 
     // Update is called once per frame
     void Update()
     {
+        var seeds = Objects.Things.Where(t => t is Seed).ToArray();
+        var seedsToAdd = new List<Seed>();
+        
+        for (var i = 0; i < beetSeedsCount - seeds.Count(s => s is BeetSeed); i++)
+        {
+            seedsToAdd.Add(new BeetSeed()
+            {
+                ThingObj = Instantiate(BeetSeed.BeetSeedPrefab, SquareSection.ConvertSectionToVector((-4, 4)),
+                    Quaternion.identity, seedsObjs.transform),
+            });
+        }
+        for (var i = 0; i < wheatSeedsCount - seeds.Count(s => s is WheatSeed); i++)
+        {
+            seedsToAdd.Add(new WheatSeed()
+            {
+                ThingObj = Instantiate(WheatSeed.WheatSeedPrefab, SquareSection.ConvertSectionToVector((-2, 4)),
+                    Quaternion.identity, seedsObjs.transform),
+            });
+        }
+        for (var i = 0; i < appleTreeSeedsCount - seeds.Count(s => s is AppleTreeSeed); i++)
+        {
+            seedsToAdd.Add(new AppleTreeSeed()
+            {
+                ThingObj = Instantiate(AppleTreeSeed.AppleTreeSeedPrefab, SquareSection.ConvertSectionToVector((-1, 4)),
+                    Quaternion.identity, seedsObjs.transform),
+            });
+        }
+        
+        Objects.Things.AddRange(seedsToAdd);
     }
 }
